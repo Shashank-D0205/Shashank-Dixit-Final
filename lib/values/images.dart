163129f254423ce9a8e46818e2e60395770ca47a
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2024-10-05 11:16:51 +0530

    Initial Commit

---
+++ lib/values/images.dart
@@ -0,0 +1,80 @@
+part of values;
+
+class ImagePath {
+  //images route
+  static const String imageDir = "assets/images";
+  static const String projectsDir = "assets/images/projects";
+  static const String screenshotsDir = "assets/screenshots";
+
+  //Images
+  static const String DEV = "$imageDir/cv dp.jpg";
+
+  static const String VYBZ = "$imageDir/vybz.jpg";
+  static const String Shashank = "$imageDir/cv dp.jpg";
+
+  static const String WORKS = "$imageDir/works.png";
+  static const String ProjectsBg = "$imageDir/works.png";
+
+
+
+  static const String CIRCLE = "$imageDir/circle.png";
+  static const String STATUE = "$imageDir/STATUE2.png";
+
+  static const String PLAYSTORE = "$imageDir/playstore.png";
+  static const String ARROW_RIGHT = "$imageDir/right-arrow.png";
+  static const String ARROW_DOWN = "$imageDir/down-arrow.png";
+  static const String ARROW_DOWN_2 = "$imageDir/down-arrow-2.png";
+  static const String ARROW_UP = "$imageDir/up-arrow.png";
+  static const String ARROW_DOWN_IOS = "$imageDir/ios-down-arrow.png";
+
+  static const String GOOGLE_PLAY = "$imageDir/google_play.png";
+
+  // Certifications
+  static const String AWS_CP = "$imageDir/AWS CP.png";
+  static const String AWS_SAA = "$imageDir/AWS SAA.jpg";
+  static const String IIOT = "$imageDir/IIOT.jpg";
+  static const String NPTELCC =
+      "$imageDir/NPTEL CC.jpg";
+
+  static const String CV_COVER = "$projectsDir/SDev/0.jpg";
+
+  // YuvaAnubhav
+  static const String Yuva_Anubhav_Cover =
+      "$projectsDir/YuvaAnubhav/9.png";
+  static const String Yuva_Anubhav_Screens =
+      "$projectsDir/YuvaAnubhav/9.png";
+  //
+  // static const String DISNEY_PLUS_1 =
+  //     "$projectsDir/disneyplus/disneyplus_header.png";
+  // static const String DISNEY_PLUS_2 =
+  //     "$projectsDir/disneyplus/disneyplus_cover.png";
+  // static const String DISNEY_PLUS_3 =
+  //     "$projectsDir/disneyplus/disneyplus_description.png";
+  // static const String DISNEY_PLUS_4 =
+  //     "$projectsDir/disneyplus/disneyplus_theme.png";
+  // static const String DISNEY_PLUS_5 =
+  //     "$projectsDir/disneyplus/disneyplus_mockups.png";
+  // static const String DISNEY_PLUS_6 =
+  //     "$projectsDir/disneyplus/disneyplus_designs.png";
+  // static const String DISNEY_PLUS_7 =
+  //     "$projectsDir/disneyplus/disneyplus_more_description.png";
+  // static const String DISNEY_PLUS_8 =
+  //     "$projectsDir/disneyplus/disneyplus_downloads_feature.png";
+  // static const String DISNEY_PLUS_9 =
+  //     "$projectsDir/disneyplus/disneyplus_great_menu.png";
+  // static const String DISNEY_PLUS_10 =
+  //     "$projectsDir/disneyplus/disneyplus_profiles.png";
+  // static const String DISNEY_PLUS_11 =
+  //     "$projectsDir/disneyplus/disneyplus_home.png";
+  // static const String DISNEY_PLUS_12 =
+  //     "$projectsDir/disneyplus/disneyplus_components.png";
+  // static const String DISNEY_PLUS_13 =
+  //     "$projectsDir/disneyplus/disneyplus_the_end.png";
+
+  //
+  static const String CURIFY_COVER =
+      "$projectsDir/Curify/2.jpeg";
+  static const String CURIFY_SCREENS =
+      "$projectsDir/Curify/1.jpeg";
+  //
+}
